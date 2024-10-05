33eea09aba78d356281d16e024f54722840478c6
bharani2624 <bharani2624baru@gmail.com>
2024-10-05 15:07:07 +0530

    Made Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import '../src/animations/animate.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './assets/components/Header';
+import Achievements from './assets/components/Achievements';
+import Content from './assets/components/Content';
+import Footer from './assets/components/Footer';
 import loader from '../src/assets/enhance/omnitrix.gif'
 function App() {
+  const[isLoading,setIsLoading]=useState(true);
+  useEffect(()=>
+    {
+      const timer =setTimeout(()=>
+        {
+          setIsLoading(false);
+        },10000);
+        return ()=> clearTimeout(timer);
+    },[])
   return (
    <div className="App">
-    <Router>
+    {isLoading?(
+      <div className="loader flex flex-col items-center justify-center space-y-5 h-screen w-screen">
+        <img src={loader} className='w-32 h-32' alt="" />
+        <div className="text-green-600 font-grobold text-right">
+          Loading...
+        </div>
+      </div>
+    ):(
+      <Router>
       <Routes>
       <Route path='/' element={<Header/>}/>
+      <Route path='/Achievements' element={<Achievements/>}/>
+      <Route path='/Content' element={<Content/>}/>
+      <Route path='/Footer' element={<Footer/>}/>
       </Routes>
     </Router>
+    )}
    </div>
   )
 }
---
+++ src/assets/components/Achievements.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+const Achievements=()=>
+    {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+export default Achievements;
