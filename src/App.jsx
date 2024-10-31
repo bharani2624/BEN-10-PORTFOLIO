e4a3538c18a264facbf0cab0e7dd4d766a5c319b
bharani2624 <bharani2624baru@gmail.com>
2024-10-31 22:43:24 +0530

    Made Some Changes In Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import '../src/animations/animate.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 import Achievements from './assets/components/Achievements';
 import About from './assets/components/About';
 import Content from './assets/components/Content';
@@ -14,11 +14,12 @@ import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
 import CircularMenu from './assets/components/CircularMenu/CircularMenu';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [quote, setQuote] = useState('');
-  const [mousePosition,setmousePosition]=useState({x:0,y:0});
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const quotes = [
     "It's Hero Time!",
@@ -27,56 +28,53 @@ function App() {
     "Heroes never give up.",
     "I can’t always win, but I never give up."
   ];
-  const handleMouseMovement=(event)=>
-    {
-      const {clientX,clientY}=event;
-      setmousePosition({x:clientX,y:clientY});
-    }
 
+  const handleMouseMovement = (event) => {
+    const { clientX, clientY } = event;
+    setMousePosition({ x: clientX, y: clientY });
+  };
 
-  useEffect(()=>{
-    const timer=setTimeout(()=>
-      {
-        setIsLoading(false);
-      },5000)
-      const quoteTimer=setInterval(()=>
-        {
-          const randomQuote=quotes[Math.floor(Math.random()*quotes.length)];
-          setQuote(randomQuote);
-        },1000);
-        window.addEventListener('mousemove',handleMouseMovement)
-        return()=>
-          {
-            clearTimeout(timer);
-            clearInterval(quoteTimer);
-            window.removeEventListener('mousemove',handleMouseMovement);
-          }
-  },[])
-  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    const quoteTimer = setInterval(() => {
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      setQuote(randomQuote);
+    }, 1000);
+    window.addEventListener('mousemove', handleMouseMovement);
+    
+    return () => {
+      clearTimeout(timer);
+      clearInterval(quoteTimer);
+      window.removeEventListener('mousemove', handleMouseMovement);
+    };
+  }, []);
+
   const particlesInit = useCallback(async engine => {
     console.log(engine);
     await loadSlim(engine);
-}, []);
+  }, []);
 
-const particlesLoaded = useCallback(async container => {
+  const particlesLoaded = useCallback(async container => {
     await console.log(container);
-}, []);
-  
-  const backgroundPosition=
-  {
-    backgroundPosition:`${mousePosition.x/150}px ${mousePosition.y/100}px`,
-    backgroundImage:`url(${pod})`,
-    backgroundSize:'cover',
-    backgroundRepeat:'no-repeat'
-    
-  }
+  }, []);
+
+  const backgroundPosition = {
+    backgroundPosition: `${mousePosition.x / 150}px ${mousePosition.y / 100}px`,
+    backgroundImage: `url(${pod})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  };
+
   return (
+    <BrowserRouter>
     <div className="App">
       {isLoading ? (
         <div className="loader flex flex-col items-center justify-center w-screen">
-          <div className=" absolute inset-0 bg-cover bg-center filter blur-md" style={backgroundPosition}></div>
+          <div className="absolute inset-0 bg-cover bg-center filter blur-md" style={backgroundPosition}></div>
           <div className="relative w-9/12 h-28 md:w-6/12 md:h-24 lg:w-6/12 space-y-2 flex flex-col items-center justify-center text-green-600 text-4xl text-center mt-4 border-spacing-5 lg:h:24 ">
-            {quote?quote:"I am Just Being Over Confident."} <br />
+            {quote ? quote : "I am Just Being Over Confident."} <br />
             <span className='text-bold font-grobold text-2xl text-ben10'>
               -BEN<span className='text-white border-black border-opacity-25 font-grobold'>10</span>
             </span>
@@ -86,9 +84,8 @@ const particlesLoaded = useCallback(async container => {
           </div>
         </div>
       ) : (
-        <Router>
-            <div>
-            <Particles className=""
+        <div>
+          <Particles className=""
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
@@ -107,7 +104,7 @@ const particlesLoaded = useCallback(async container => {
                   },
                   onHover: {
                     enable: true,
-                    mode:"repulse",
+                    mode: "repulse",
                   },
                   resize: true,
                 },
@@ -116,130 +113,147 @@ const particlesLoaded = useCallback(async container => {
                     quantity: 0,
                   },
                   repulse: {
-                            distance: 500,
-                            duration: 0.5,
-                          },
-                        },
-                      },
-                      particles: {
-                        color: {
-                          value: "#ffffff",
-                        },
-                        links: {
-                          color: "#3857",
-                          distance: 500,
-                          enable: true,
-                          opacity: 0.5,
-                          width: 0.5,
-                        },
-                        move: {
-                          direction: "none",
-                          enable: true,
-                          outModes: {
-                            default: "bounce",
-                          },
-                          random: false,
-                          speed: 10,
-                          straight: false,
-                        },
-                        number: {
-                          density: {
-                            enable: true,
-                            area: 900,
-                          },
-                          value: 50,
-                        },
-                        opacity: {
-                        value: 0.5,
-                      },
-                      shape: {
-                        type: "square",
-                      },
-                      size: {
-                        value: { min: 0, max: 1 },
-                      },
-                    },
-                detectRetina: true,
-                emitters: [
-                  {
-                    direction: "top",
-                    position: {
-                      x: 50,
-                      y: 0,
-                    },
-                    rate: {
-                      quantity: 10,
-                      delay: 0.1,
-                    },
-                    size: {
-                      width: 100,
-                      height: 0,
-                    },
+                    distance: 500,
+                    duration: 0.5,
                   },
-                  {
-                    direction: "bottom",
-                    position: {
-                      x: 50,
-                      y: 100,
-                    },
-                    rate: {
-                      quantity: 0,
-                      delay: 0.1,
-                    },
-                    size: {
-                      width: 100,
-                      height: 0,
-                    },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#ffffff",
+                },
+                links: {
+                  color: "#3857",
+                  distance: 500,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 0.5,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
                   },
-                  {
-                    direction: "left",
-                    position: {
-                      x: 50,
-                      y: 50,
-                    },
-                    rate: {
-                      quantity: 0,
-                      delay: 0.1,
-                    },
-                    size: {
-                      width: 0,
-                      height: 100,
-                    },
+                  random: false,
+                  speed: 10,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 900,
                   },
-                  {
-                    direction: "right",
-                    position: {
-                      x: 100,
-                      y: 50,
-                    },
-                    rate: {
-                      quantity: 100,
-                      delay: 0.1,
-                    },
-                    size: {
-                      width: 0,
-                      height: 100,
-                    },
+                  value: 50,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "square",
+                },
+                size: {
+                  value: { min: 0, max: 1 },
+                },
+              },
+              detectRetina: true,
+              emitters: [
+                {
+                  direction: "top",
+                  position: {
+                    x: 50,
+                    y: 0,
                   },
-                ],
-              }}
-              />
+                  rate: {
+                    quantity: 10,
+                    delay: 0.1,
+                  },
+                  size: {
+                    width: 100,
+                    height: 0,
+                  },
+                },
+                {
+                  direction: "bottom",
+                  position: {
+                    x: 50,
+                    y: 100,
+                  },
+                  rate: {
+                    quantity: 0,
+                    delay: 0.1,
+                  },
+                  size: {
+                    width: 100,
+                    height: 0,
+                  },
+                },
+                {
+                  direction: "left",
+                  position: {
+                    x: 50,
+                    y: 50,
+                  },
+                  rate: {
+                    quantity: 0,
+                    delay: 0.1,
+                  },
+                  size: {
+                    width: 0,
+                    height: 100,
+                  },
+                },
+                {
+                  direction: "right",
+                  position: {
+                    x: 100,
+                    y: 50,
+                  },
+                  rate: {
+                    quantity: 100,
+                    delay: 0.1,
+                  },
+                  size: {
+                    width: 0,
+                    height: 100,
+                  },
+                },
+              ],
+            }}
+          />
+          <nav>
+            <ul className="flex space-x-4">
+              <li><Link to="Content" smooth={true} duration={500}>Content</Link></li>
+              <li><Link to="About" smooth={true} duration={500}>About</Link></li>
+              <li><Link to="Achievements" smooth={true} duration={500}>Achievements</Link></li>
+              <li><Link to="Skills" smooth={true} duration={500}>Skills</Link></li>
+              <li><Link to="Projects" smooth={true} duration={500}>Projects</Link></li>
+              <li><Link to="Footer" smooth={true} duration={500}>Footer</Link></li>
+            </ul>
+          </nav>
+          <Element name="Content" className="element">
             <Content />
+          </Element>
+          <Element name="About" className="element">
             <About />
-            <Achievements/>
-            <Skills/>
-            <Projects/>
-            <Footer/>
-          <CircularMenu/>
-          </div>
-          <Routes>
-            <Route path="/Achievements" element={<Achievements />} />
-            <Route path="/Content" element={<Content />} />
-            <Route path="/Footer" element={<Footer />} />
-          </Routes>
-        </Router>
+          </Element>
+          <Element name="Achievements" className="element">
+            <Achievements />
+          </Element>
+          <Element name="Skills" className="element">
+            <Skills />
+          </Element>
+          <Element name="Projects" className="element">
+            <Projects />
+          </Element>
+          <Element name="Footer" className="element">
+            <Footer />
+          </Element>
+          <CircularMenu />
+        </div>
       )}
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/assets/components/CircularMenu/CircularMenu.jsx
+++ src/assets/components/CircularMenu/CircularMenu.jsx
@@ -1,51 +1,71 @@
 import React from "react";
 import { useState } from "react";
 import useSound from 'use-sound';
-import omnitrixAudio from '../CircularMenu/OmnitrixAudio.mp3'; // Correct path to sound file
-import omnitrix from '../CircularMenu/O100.png'
-import omnitrixOn from '../CircularMenu/omnitrixOn.png'
+import { useNavigate } from 'react-router-dom';
+import omnitrixAudio from '../CircularMenu/OmnitrixAudio.mp3';
+import omnitrix from '../CircularMenu/O100.png';
+import omnitrixOn from '../CircularMenu/omnitrixOn.png';
 import af1 from '../CircularMenu/AlienFace/DiamondHead.png';
 import af2 from '../CircularMenu/AlienFace/Waybig-removebg-preview.png';
 import af3 from '../CircularMenu/AlienFace/HeatBlast.png';
 import af4 from '../CircularMenu/AlienFace/FourArms.png';
-import './CircularMenu.css'
-const CircularMenu=()=>
-    {
-        const [playSound]=useSound(omnitrixAudio);
-        return(
-            <div className="flex items-center justify-center" onClick={playSound}>
-            <div className="menu fixed  bg-green-600 bottom-20 right-10" >
+import './CircularMenu.css';
+
+const CircularMenu = () => {
+    const [playSound] = useSound(omnitrixAudio);
+    const history=useNavigate();
+    const skill=()=>
+        {
+            history('/Achievements');
+            console.log("Function Is Working");
+        }
+    return (
+        <div className="flex items-center justify-center" onClick={playSound}>
+            <div className="menu fixed bg-green-600 bottom-5 right-5">
                 <input type="checkbox" id="toggle" />
                 <label htmlFor="toggle" id="show-menu">
                     <div className="btn">
-                    {/* <i class="fa-solid fa-bomb menubtn" className="menubtn"></i> */}
-                    <img src={omnitrix} className="menubtn" onClick={playSound} alt="" />
-                    {/* <i class="fa-solid fa-wand-magic-sparkles closebtn" className="closebtn"></i> */}
-                    <img src={omnitrixOn} className="closebtn" alt="" />
+                        <img src={omnitrix} className="menubtn" onClick={playSound} alt="" />
+                        <img src={omnitrixOn} className="closebtn" alt="" />
                     </div>
-                    <div className="btn">
-                        {/* <i class="fa-solid fa-pen"></i> */}
-                        <img src={af1} className="w-12" alt="" />
-                        </div>
-                    <div className="btn">
-                        {/* <i class="fa-solid fa-pen"></i> */}
-                        <img src={af2} className="w-12" alt="" />
-                        
-                        </div>
-                    <div className="btn">
-                        {/* <i class="fa-solid fa-pen"></i> */}
-                        <img src={af3} className="w-10" alt="" />
-
-                        </div>
-                    <div className="btn">
-                        {/* <i class="fa-solid fa-pen"></i> */}
-                        <img src={af4} className="w-10" alt="" />
-                        
+                    
+                    <div className="btn relative group">
+                        <img src={af1} className="i1 w-12" alt="" />
+                        <span className="absolute left-1/2 mb-2 w-full h-full flex items-center justify-center -translate-x-1/2 p-2 text-sm text-white bg-green-700/50 blur-0 rounded z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            Achievements
+                        </span>
                     </div>
-                    <div className="btn"><i class="fa-solid fa-pen"></i></div>
+                    
+                    <div className="btn relative group">
+                        <img src={af2} className="i2 w-12" alt=""  />
+                        <span className="absolute left-1/2 mb-2 w-full h-full flex items-center justify-center -translate-x-1/2 p-2 text-sm text-white bg-green-700/50 blur-0 rounded z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300" onClick={skill}>
+                            Skills
+                        </span>
+                    </div>
+                    
+                    <div className="btn relative group">
+                        <img src={af3} className="i3 w-10" alt="" />
+                        <span className="absolute left-1/2 mb-2 w-full h-full flex items-center justify-center -translate-x-1/2 p-2 text-sm text-white bg-green-700/50 blur-0 rounded z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            Projects
+                        </span>
+                    </div>
+                    
+                    <div className="btn relative group">
+                        <img src={af4} className="i4 w-10" alt="" />
+                        <span className="absolute left-1/2 mb-2 w-full h-full flex items-center justify-center -translate-x-1/2 p-2 text-sm text-white bg-green-700/50 blur-0 rounded z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            Contacts
+                        </span>
+                    </div>
+                    <div className="btn relative group">
+                        <img src={af4} className="i4 w-10" alt="" />
+                        <span className="absolute left-1/2 mb-2 w-full h-full flex items-center justify-center -translate-x-1/2 p-2 text-sm text-white bg-green-700/50 blur-0 rounded z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            Contacts
+                        </span>
+                    </div>
                 </label>
             </div>
-            </div>
-        )
-    }
+        </div>
+    );
+}
+
 export default CircularMenu;
